3a5a33598d8134d5f4691b257199fe0be3852bb0
Jozef Sovcik <jozef@sovcik.net>
2019-10-15 15:43:26 +0200

    moving variables to heap

--- src/Logger.cpp
+++ src/Logger.cpp
@@ -48,57 +48,60 @@ void Logger::getISOTime(char *buff, size_t size) {
     snprintf(buff, size, "%d-%02d-%02dT%02d:%02d:%02dZ", year(), month(), day(), hour(), minute(), second());
 }
 
-
-void Logger::log(LogLevel level, const char* module, const char* text, ...) {
-    LogRecord rec;
-    bool useBuffer = true;
-    
+void Logger::log(LogLevel level, const char* module, const char* text, ...){   
     if (level > activeLevel) return;
+
+    LogRecord *rec = new LogRecord;
 
     va_list argptr;
     va_start(argptr, text);
-    vsnprintf(&rec.text[0], BUFFER_RECORD_TEXT_SIZE, text, argptr);
+    vsnprintf(rec->text, BUFFER_RECORD_TEXT_SIZE, text, argptr);
     va_end(argptr);
 
-    getISOTime(&rec.datetime[0],BUFFER_RECORD_DATE_SIZE);
+    getISOTime(rec->datetime, BUFFER_RECORD_DATE_SIZE);
 
-    DEBUG_PRINT("[logger:log] going to log date=%s, level=%s, module=%s, text=%s\n", rec.datetime, LogLevelStrings[level], module, &rec.text[0]);
+    DEBUG_PRINT("[logger:logP] going to log date=%s, level=%s, module=%s, text=%s\n", rec->datetime, LogLevelStrings[level], module, rec->text);
+
+    strncpy(rec->level, LogLevelStrings[level], BUFFER_RECORD_LEVEL_SIZE);
+    rec->level[BUFFER_RECORD_LEVEL_SIZE-1]=0;
+    strncpy_P(rec->module, module, BUFFER_RECORD_MODULE_SIZE);
+    rec->module[BUFFER_RECORD_MODULE_SIZE-1]=0;
 
     // in order to make sure log records will be written (e.g. sent to server) in the right order
     // write directly only if buffer is already empty
+    bool useBuffer = true;
     if (!logBuffer || logBuffer->isEmpty()){
         if (logWriter) {
-            useBuffer = logWriter->writeLogEntry(&rec.datetime[0], LogLevelStrings[level], module, &rec.text[0]) == 0;
+            useBuffer = logWriter->writeLogEntry(rec->datetime, rec->level, rec->module, rec->text) == 0;
             if (useBuffer) 
                 DEBUG_PRINT("[logger:log] log writing failed\n");
         } else {
             DEBUG_PRINT("[logger:log] writer not configured.\n");
         }
-    }
+    }    
 
     if (useBuffer && logBuffer) { 
-        DEBUG_PRINT("[logger:log] going to use buffer\n");
-        strncpy(&rec.level[0], LogLevelStrings[level], BUFFER_RECORD_LEVEL_SIZE);
-        rec.level[BUFFER_RECORD_LEVEL_SIZE-1]=0;
-        strncpy(&rec.module[0], module,BUFFER_RECORD_MODULE_SIZE);
-        rec.module[BUFFER_RECORD_MODULE_SIZE-1]=0;
+        DEBUG_PRINT("[logger:logP] going to use buffer\n");
+
         //DEBUG_PRINT("[logger:log] date=%s, module=%s, text=%s\n",rec.datetime, rec.module, rec.text);
-        logBuffer->write(&rec);
+        logBuffer->write(rec);
     }
 
+    delete rec;
 }
 
 void Logger::processBuffer(){
     if (!logBuffer || logBuffer->isEmpty() || !logWriter) 
         return;
 
-    LogRecord rec;
-    if (logBuffer->read(&rec)){  // if there is new record
-        DEBUG_PRINT("[logger:proLog] processing buffered log entry. date=%s, text=%s\n",&rec.datetime[0], &rec.text[0]);
-        int r = logWriter->writeLogEntry( &rec.datetime[0], &rec.level[0], &rec.module[0], &rec.text[0]);
+    LogRecord *rec = new LogRecord;
+    if (logBuffer->read(rec)){  // if there is new record
+        DEBUG_PRINT("[logger:proLog] processing buffered log entry. date=%s, text=%s\n",rec->datetime, rec->text);
+        int r = logWriter->writeLogEntry( rec->datetime, rec->level, rec->module, rec->text);
         if (!r) // TODO: if write failed - put it back to log?
             DEBUG_PRINT("[logger:proLog] failed writing log entry - log entry lost\n");
     }
+    delete rec;
 
 }
 
